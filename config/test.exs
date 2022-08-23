73b953312ea662eb4689f367cf9becb77d7232b4
Sam Rose <sam@floxdev.com>
2022-08-23 17:07:13 -0400

    hello phoenix

---
+++ config/test.exs
@@ -0,0 +1,28 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :hello, Hello.Repo,
+  socket: Path.absname("../postgres/.s.PGSQL.5435"),
+  database: "hello_test#{System.get_env("MIX_TEST_PARTITION")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :hello, HelloWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "jxNhUuiVNX/BaIJuv7AT/SOlwJHqQNEbFOKl4tjWb56jKc2/4OzVeszX45IN7yPe",
+  server: false
+
+# In test we don't send emails.
+config :hello, Hello.Mailer, adapter: Swoosh.Adapters.Test
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
